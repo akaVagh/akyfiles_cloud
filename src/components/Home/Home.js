f4bfc5977a5a73446cea4dc54667ea54d9ff9fb8
Harsh Vaghani <61564775+akaVagh@users.noreply.github.com>
2023-04-12 12:14:50 -0300

    Final commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Home.module.css';
-
+import arcimg from '../../ProjectArc.jpg';
 const Home = () => {
-	return <div className={styles.Home}>Home</div>;
+	return (
+		<div className={styles.Home}>
+			<h1>Application Architecture</h1>
+			<img src={arcimg} className={styles.center} />
+		</div>
+	);
 };
 Home.propTypes = {};
 
